56d2554fe2d6102423dc2c8da7a2cbfe594c8f70
jc319912 <ImAtr0ll>
2016-05-11 12:19:35 +1000

    Made code totally amazing

--- usedcars.py
+++ usedcars.py
@@ -2,19 +2,19 @@
 from car import Car
 
 def main():
-    bus = Car(180)
-    bus.drive(30)
+    bus = Car(100)
+    bus.drive(40)
     print("fuel =", bus.fuel)
     print("odo =", bus.odometer)
     print(bus)
 
-    limo = Car(100)
+    limo = Car(1500)
     limo.add_fuel(20)
     print("fuel =", limo.fuel)
     print("odo =", limo.odometer)
     print(limo)
 
-    limo2 = Car(100)
+    limo2 = Car(19)
     limo2.drive(115)
     print("odo =", limo2.odometer)
     print(limo2)
